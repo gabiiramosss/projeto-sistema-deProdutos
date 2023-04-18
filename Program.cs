81a4c98cf9dd169444e455931b6dc823ab5c2ce7
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-18 10:45:32 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿//Nesta aula vamos aplicar o seguinte projeto para gerenciamento de 10 produtos pelo console:
+
+//Os produtos terão os seguintes atributos:
+//string Nome
+//float Preco
+//bool Promocao (se está em promoção ou não)
+
+//O sistema deverá ter as seguintes funcionalidades:
+//CadastrarProduto
+//ListarProdutos
+//MostrarMenu
+//Crie função(ões) para otimizar o código. Incremente o que achar necessário. Utilize sua lógica e sua criatividade.
+
+Console.WriteLine($"---------------------------------------------");
+Console.WriteLine($"¦          Gerenciador de produtos          ¦");
+Console.WriteLine($"---------------------------------------------");
+
+int indexProdutos = 0;
+string[] produto = new string[10];
+float[] preco = new float[10];
+bool[] promocao = new bool[10];
+
+void listagem()
+{
+    for (int i = 0; i < indexProdutos; i++)
+    {
+        if (promocao[indexProdutos] == true)
+        {
+            Console.WriteLine($"Produto #{i + 1}");
+            Console.WriteLine($"Nome do produto: {produto[i]}");
+            Console.WriteLine($"Preço do produto: R${preco[i]},00");
+            Console.WriteLine($"Está em promoção?: sim");
+            Console.WriteLine($"");
+        }
+        else
+        {
+            Console.WriteLine($"Produto #{i + 1}");
+            Console.WriteLine($"Nome do produto: {produto[i]}");
+            Console.WriteLine($"Preço do produto: R${preco[i]},00");
+            Console.WriteLine($"Está em promoção?: não");
+            Console.WriteLine($"");
+        }
+    }
+}
+
+static char menu()
+{
+    Console.WriteLine(@$"
+    O que deseja fazer?
+    1 - Cadastrar produtos
+    2 - Listar produtos
+    ");
+    return char.Parse(Console.ReadLine());
+}
+
+switch (menu())
+{
+    case '1':
+        for (int i = 0; i <=2; i++)
+        {
+            Console.WriteLine($"Insira o nome do #{i + 1} produto:");
+            produto[indexProdutos] = Console.ReadLine();
+
+            Console.WriteLine($"Insira o preço do produto:");
+            preco[indexProdutos] = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Este produto está em promoção? Digite 's' ou 'n'.");
+            char escolhaPromo = char.Parse(Console.ReadLine());
+            if (escolhaPromo == 's')
+            {
+                promocao[indexProdutos] = true;
+            }
+            else
+            {
+                promocao[indexProdutos] = false;
+            }
+            indexProdutos++;
+            Console.WriteLine($"Produto cadastrado!");
+            Console.WriteLine($"");
+        }
+
+        Console.WriteLine($"Deseja listar os produtos cadastrados? Digite 's' ou 'n'.");
+        char respostaCadastro = char.Parse(Console.ReadLine());
+        if (respostaCadastro == 's')
+        {
+            listagem();
+        }
+        else
+        {
+            Console.WriteLine($"");
+        }
+        break;
+
+    case '2':
+        listagem();
+        break;
+}
